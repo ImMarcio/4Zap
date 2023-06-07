d0b41fc4d283e4ca7ac978c7f628d274b69211e1
Marcio Jose da Silva <20221370023@ad.ifpb.edu.br>
2023-06-07 15:21:35 -0300

    Criando os atributos da classe participante e Mensagem e seus métodos básicos

--- system-4TALK/src/repositorio/Repositorio.java
+++ system-4TALK/src/repositorio/Repositorio.java
@@ -1,5 +1,10 @@
 package repositorio;
 
 public class Repositorio {
+    private TreeMap<String,Participante> participantes = new TreeMap<>();
+    private TreeMap<Integer,Mensagem> mensagens = new TreeMap<>();
 
+    public Repositorio(){
+        carregarObjetos(); //ler dados dos arquivos
+    }
 }
