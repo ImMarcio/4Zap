8bd473eca3b75849b25a8f04231dff5ef50ad992
Márcio <marciojsilva159@gmail.com>
2023-06-10 22:58:48 -0300

    Criando o método "carregarObjetos"
    Esse métodos carrega grupos, participantes e mensanges.

--- system-4TALK/src/repositorio/Repositorio.java
+++ system-4TALK/src/repositorio/Repositorio.java
@@ -1,7 +1,12 @@
 package repositorio;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.TreeMap;
 
+import modelo.Grupo;
 import modelo.Individual;
 import modelo.Mensagem;
 import modelo.Participante;
@@ -11,15 +16,107 @@ public class Repositorio {
     private TreeMap<Integer,Mensagem> mensagens = new TreeMap<>();
 
     public Repositorio(){
-        //carregarObjetos(); //ler dados dos arquivos
+        carregarObjetos(); //ler dados dos arquivos
     }
     
-    // Adicionar um índividuo novo
-	public void adicionarIndividuo(Individual i){
+    // Adicionar um índividuo ou um grupo novo
+	public void adicionarParticipante(Participante i){
 		this.participantes.put(i.getNome(), i);
 	}
 	// Localizar um individual no TreeMap participantes
 	public Individual localizarIndividuo(String nome){
 		return (Individual) participantes.get(nome);
 	}
+	
+	
+	
+	public void carregarObjetos() {
+		try {
+			File arquivoDeIndividuo = new File( new File(".\\individual.csv").getCanonicalPath() ) ; 
+			File arquivoDoGrupo = new File( new File(".\\grupo.csv").getCanonicalPath() ) ; 
+			File arquivoDeMensagem = new File( new File(".\\mensagem.csv").getCanonicalPath() ) ;
+			if (!arquivoDeIndividuo.exists() || !arquivoDoGrupo.exists() || arquivoDeMensagem.exists()  ) {
+				//System.out.println("criando arquivo .csv vazio");
+				FileWriter arquivo1 = new FileWriter(arquivoDeIndividuo); arquivo1.close();
+				FileWriter arquivo2 = new FileWriter(arquivoDoGrupo); arquivo2.close();
+				FileWriter arquivo3 = new FileWriter(arquivoDeMensagem); arquivo3.close();
+				return;
+			}
+		}
+		catch(Exception ex)		{
+			throw new RuntimeException("criacao dos arquivos vazios:"+ex.getMessage());
+		}
+		String linha;
+		String[] partes;
+		String nomeindividuo,senha,nomegrupo;
+		ArrayList<String> nomeDosParticipantes = null;
+		boolean admistrador;
+		Individual individuo;
+		Grupo grupo;
+		Scanner arquivo = null;
+		
+		try {
+			File f = new File( new File(".\\individual.csv").getCanonicalPath() );
+			arquivo = new Scanner(f); // pasta do objeto
+			while(arquivo.hasNextLine()) {
+				linha = arquivo.nextLine().trim();		
+				partes = linha.split(";");	
+				nomeindividuo = partes[0];
+				senha = partes[1];
+				admistrador = Boolean.getBoolean(partes[3]);
+			
+				individuo = new Individual(nomeindividuo, senha, admistrador);
+				this.adicionarParticipante(individuo);
+				
+			}
+			arquivo.close();
+			
+		}
+		catch(Exception ex) {
+			throw new RuntimeException("leitura arquivo de individuos:"+ex.getMessage());
+		}
+		
+		try {
+			File f = new File( new File(".\\grupo.csv").getCanonicalPath() );
+			arquivo = new Scanner(f); // pasta do objeto
+			while(arquivo.hasNextLine()) {
+				linha = arquivo.nextLine().trim();		
+				partes = linha.split(";");
+				nomegrupo = partes[0];
+				grupo = new Grupo(nomegrupo);
+				for(int i = 1; i < partes.length; i++) {
+					grupo.adicionar(this.localizarIndividuo(partes[i]));
+				}
+			
+				this.adicionarParticipante(grupo);
+				
+				
+			}
+			
+		}
+		catch(Exception ex) {
+			
+		}
+		
+		
+	}	
+	
+//	linha = arquivo.nextLine().trim();		
+//	partes = linha.split(";");
+//	nomegrupo = partes[0];
+//	nomeDosParticipantes =  "Participantes: " + '\n';
+//	for(int i = 1; i < partes.length; i++) {
+//		nomeDosParticipantes =  partes[i] + '\n';
+//	}
+//	
+//	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
